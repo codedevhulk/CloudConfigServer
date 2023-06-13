e43d7ad1e1b993f4879261a50e17ec27bbd67e0c
Rajesh Gottimukkula <rajesh.gottimukkula1@gmail.com>
2023-06-13 15:51:10 +0530

    Added configuaration server

---
+++ src/main/java/com/cloudConfig/cloudConfigServer/CloudConfigServerApplication.java
@@ -0,0 +1,16 @@
+package com.cloudConfig.cloudConfigServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableConfigServer
+public class CloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudConfigServerApplication.class, args);
+	}
+
+}
